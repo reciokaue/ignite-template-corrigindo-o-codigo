6318a79b47fa3c79550fd4839098dc8ff74b535e
Kaue Recio <kaue.recio2@gmail.com>
2022-08-02 07:20:51 -0300

    Passing every test

--- src/index.js
+++ src/index.js
@@ -22,25 +22,31 @@ app.post("/repositories", (request, response) => {
     techs,
     likes: 0
   };
+  repositories.push(repository)
 
-  return response.json(repository);
+  return response.json(repository).status(201);
 });
 
 app.put("/repositories/:id", (request, response) => {
   const { id } = request.params;
-  const updatedRepository = request.body;
+  const {title, url, techs} = request.body;
 
-  repositoryIndex = repositories.findindex(repository => repository.id === id);
-
+  repositoryIndex = repositories.findIndex(repository => repository.id === id);
+  
   if (repositoryIndex < 0) {
     return response.status(404).json({ error: "Repository not found" });
   }
+    
+  const newRepository = {
+    id,
+    title,
+    url,
+    techs,
+    likes: repositories[repositoryIndex].likes
+  }
+  repositories[repositoryIndex] = newRepository;
 
-  const repository = { ...repositories[repositoryIndex], ...updatedRepository };
-
-  repositories[repositoryIndex] = repository;
-
-  return response.json(repository);
+  return response.json(newRepository);
 });
 
 app.delete("/repositories/:id", (request, response) => {
@@ -48,7 +54,7 @@ app.delete("/repositories/:id", (request, response) => {
 
   repositoryIndex = repositories.findIndex(repository => repository.id === id);
 
-  if (repositoryIndex > 0) {
+  if (repositoryIndex < 0) {
     return response.status(404).json({ error: "Repository not found" });
   }
 
@@ -67,8 +73,10 @@ app.post("/repositories/:id/like", (request, response) => {
   }
 
   const likes = ++repositories[repositoryIndex].likes;
+  console.log(likes)
+  repositories[repositoryIndex].likes = likes
 
-  return response.json('likes');
+  return response.json({likes: repositories[repositoryIndex].likes}).status(204);
 });
 
 module.exports = app;
